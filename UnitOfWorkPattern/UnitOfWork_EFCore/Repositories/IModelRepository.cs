50e0a4988539ad3e08865cefadfc28d515c1e2fc
AmirHoseinRahmati23 <www.amir233@gmail.com>
2023-11-06 18:47:44 +0330

    Categorizing IModelRep And ModelRep

--- UnitOfWorkPattern/UnitOfWork_EFCore/Repositories/IModelRepository.cs
+++ UnitOfWorkPattern/UnitOfWork_EFCore/Repositories/IModelRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnitOfWork_EFCore
+namespace UnitOfWork_EFCore.Repositories
 {
     public interface IModelRepository
     {
--- UnitOfWorkPattern/UnitOfWork_EFCore/Services/ModelRepository.cs
+++ UnitOfWorkPattern/UnitOfWork_EFCore/Services/ModelRepository.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnitOfWork_EFCore.Base;
+using UnitOfWork_EFCore.Repositories;
 using UnitOfWork_EFCore.Temp;
 
-namespace UnitOfWork_EFCore
+namespace UnitOfWork_EFCore.Services
 {
-    public class ModelRepository: Repository<Model>, IModelRepository
+    public class ModelRepository : Repository<Model>, IModelRepository
     {
         internal ModelRepository(DatabaseContext databaseContext) : base(databaseContext)
         {
