1f2725093e2d8b9866339cb1eb57aabfe99da926
AmirHoseinRahmati23 <www.amir233@gmail.com>
2023-11-06 17:47:43 +0330

    Adding IModelRepository and ModelRepository

---
+++ UnitOfWorkPattern/UnitOfWork_EFCore/ModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitOfWork_EFCore.Base;
+using UnitOfWork_EFCore.Temp;
+
+namespace UnitOfWork_EFCore
+{
+    public class ModelRepository: Repository<Model>, IModelRepository
+    {
+        internal ModelRepository(DatabaseContext databaseContext) : base(databaseContext)
+        {
+
+        }
+    }
+}
---
+++ UnitOfWorkPattern/UnitOfWork_EFCore/temp/Model.cs
@@ -0,0 +1,13 @@
+﻿using Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitOfWork_EFCore.Temp
+{
+    public class Model: Entity
+    {
+    }
+}
