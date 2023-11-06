28d2d410df9cdf5c3923bf85a4c44e54807d20f3
AmirHoseinRahmati23 <www.amir233@gmail.com>
2023-11-06 16:34:02 +0330

    Adding Generic IRepository

---
+++ UnitOfWorkPattern/UnitOfWork_EFCore/Base/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Base;
+
+namespace UnitOfWork_EFCore.Base
+{
+    public interface IRepository<T> where T : Entity
+    {
+
+    }
+}
